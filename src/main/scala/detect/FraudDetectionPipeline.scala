8cc5a372fd7cb37a5c6f06b24fa2541fe74e9aae
mvivero091 <mvivero091@gmail.com>
2015-06-01 12:08:03 -0700

    print column names, exp analysis

--- src/main/scala/detect/FraudDetectionPipeline.scala
+++ src/main/scala/detect/FraudDetectionPipeline.scala
@@ -2,6 +2,8 @@ package detect
 
 import io.prediction.workflow.FakeRun
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SQLContext
 
 object FraudDetectionPipeline extends FakeRun {
 
@@ -9,9 +11,15 @@ object FraudDetectionPipeline extends FakeRun {
 
   def main (sc: SparkContext): Unit = {
 
-    // Import data from Parquet file.
-    
+    // Initialize SQLContext requirements.
+    val sql : SQLContext = new SQLContext(sc)
+    import sql.implicits._
 
+    // Import data from Parquet file.
+    val data : DataFrame = sql.parquetFile("/data/fraud-detection/all.parquet")
+
+    // Show data format
+    data.columns.foreach(println)
 
   }
 
